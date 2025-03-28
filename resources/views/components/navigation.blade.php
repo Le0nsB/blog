55806796eca0d5f76d908250320c3b05c45ed55a
ipb23.l.bluks <ipb23.l.bluks@pjvt.local>
2025-03-28 12:27:23 +0200

    MMM PADDING

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,7 +1,6 @@
 <header>
     <nav class="navbar">
         <ul class="route">
-            <li><img src="https://b.fssta.com/uploads/application/nba/headshots/1120.vresize.350.350.medium.27.png" class="lebron"></li>
             <li><a href="/" >Sākums</a></li>
             <li><a href="/posts/create" >Izveidot blogu</a></li>
             <li><a href="/categories" >Kategorijas</a></li>
